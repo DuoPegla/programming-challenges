d6f1c4c9738dcc8148ea3da0ce8f80d4b3658839
DuoPegla <josip.sarlija@gmail.com>
2015-10-23 16:42:50 +0200

    Updates solution for diagonal difference challenge

--- HackerRank/algorithms/warmup/diagonal-difference/diagonal-difference.py
+++ HackerRank/algorithms/warmup/diagonal-difference/diagonal-difference.py
@@ -1,10 +1,8 @@
 def diagonal_difference(matrix):
     first_diagonal_sum = 0
-    for i in range(len(matrix)):
-        first_diagonal_sum += int(matrix[i][i])
-
     second_diagonal_sum = 0
     for i in range(len(matrix)):
+        first_diagonal_sum += int(matrix[i][i])
         second_diagonal_sum += int(matrix[i][len(matrix) - 1 - i])
 
     return abs(first_diagonal_sum - second_diagonal_sum)
