abbeb36d445738e5c1f1819b57daf85e87ca39ad
DuoPegla <josip.sarlija@gmail.com>
2015-12-30 16:21:47 +0100

    Improves code formatting

--- CodeEval/challenges/easy/sum-of-primes/sum-of-primes.py
+++ CodeEval/challenges/easy/sum-of-primes/sum-of-primes.py
@@ -1,22 +1,24 @@
 import math
 
-def isPrime(number):
-	for i in range(2, int(math.ceil(math.sqrt(number))) + 1):
-		if number % i == 0:
-			return False			
-	return True
-	
-sum = 2
+
+def is_prime(number):
+    for i in range(2, int(math.ceil(math.sqrt(number))) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+
+prime_sum = 2
 count = 1
 number = 3
-while(True):
-	if isPrime(number):
-		sum += number
-		count += 1
-		
-	if (count >= 1000):
-		break
-		
-	number += 1
+while True:
+    if is_prime(number):
+        prime_sum += number
+        count += 1
 
-print sum
+    if count >= 1000:
+        break
+
+    number += 1
+
+print prime_sum
